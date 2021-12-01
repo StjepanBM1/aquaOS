54cea21e373682e42a1307dd51ebf90b3c6fad45
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2021-12-01 11:16:44 +0100

    Add files via upload

---
+++ aquaOS/Version 0.5/kernel/kernel.cpp
@@ -0,0 +1,67 @@
+#include <stdint.h>
+#include <stddef.h>
+#include "BasicRenderer.h"
+#include "cstr.h"
+#include "Framebuffer.h"
+#include "efiMemory.h"
+#include "memory.h"
+
+struct BootInfo
+{
+	Framebuffer* framebuffer;
+	PSF1_FONT* ps1_Font;
+	EFI_MEMORY_DESCRIPTOR* mMap;
+	uint64_t mMapSize;
+	uint64_t mMapDescSize;
+
+	uint64_t mReso;
+	uint64_t mResoSize;
+	uint64_t mResoDescSize;
+
+} ;
+
+
+/*/
+void Info()
+{
+
+	Framebuffer->BaseAddress = (void*)BaseAddress;
+	Framebuffer->BufferSize = BufferSize;
+	Framebuffer->Width = Height;
+	Framebuffer->Height = Width;
+	Framebuffer->PixelsPerScanLine = PixelsPerScanLine;
+}
+/*/
+
+extern "C" void _start(BootInfo* bootInfo)
+{
+	BasicRenderer newRenderer = BasicRenderer(bootInfo->framebuffer, bootInfo->ps1_Font);
+
+	newRenderer.CursorPosition = {0, 0};
+	newRenderer.Print("Welcome to aquaOS | Currently running version 0.5");
+
+	newRenderer.CursorPosition = {0, 20};
+	newRenderer.Print("(c) 2021 | Visit https://github.com/StjepanBM1/aquaOS for more informations");
+
+	newRenderer.CursorPosition = {0, 40};
+	newRenderer.Print("---------------------------------------------------------------------------");
+
+	uint64_t mMapEntries = bootInfo->mMapSize / bootInfo->mMapDescSize;
+
+	newRenderer.Print(to_string(GetMemorySize(bootInfo->mMap, mMapEntries, bootInfo->mMapDescSize)));
+
+	for (int i = 0; i < mMapEntries; i++)
+	{
+		EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)((uint64_t)bootInfo->mMap + (i * bootInfo->mMapDescSize));
+		newRenderer.CursorPosition = {0, newRenderer.CursorPosition.Y + 16};
+		newRenderer.Print(EFI_MEMORY_TYPE_STRINGS[desc->type]);
+		newRenderer.Colour = 0xffff00ff;
+		newRenderer.Print(" ");
+		newRenderer.Print(to_string(desc->numPages * 4096 / 1024));
+		newRenderer.Print(" KB");
+		newRenderer.Colour = 0xff00ffff;
+	}
+
+
+    return ;
+}
